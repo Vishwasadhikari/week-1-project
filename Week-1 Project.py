b4e6dea2fffd6e965ccf18dcd6c2ca2301d5c724
Vishwas Adhikari <141994200+Vishwasadhikari@users.noreply.github.com>
2025-11-12 19:32:49 +0530

    Add files via upload

---
+++ Week-1 Project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Personal Introduction Program
+
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+# Display a friendly message
+print("\nHi " + name + "! 👋")
+print("Wow, you're " + age + " years old and you love " + hobby + "!")
+print("It's great to meet you, " + name + "! 😊")
+
+
+# In[ ]:
+
+
+
+
